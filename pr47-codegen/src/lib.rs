cbc3362abe2f86345969a1a76eec4983dd77e01b
icey <icey@icey.tech>
2021-04-08 23:09:47 +0800

    [ pr47_codegen ] unnecessary useless update

--- pr47-codegen/src/lib.rs
+++ pr47-codegen/src/lib.rs
@@ -3,14 +3,14 @@ use proc_macro::TokenStream;
 #[proc_macro]
 pub fn pr47_test_suite(input: TokenStream) -> TokenStream {
     if !input.is_empty() {
-        let expanded = quote::quote! {
+        let expanded: TokenStream = TokenStream::from(quote::quote! {
             compile_error!("this function does not accept any parameters")
-        };
-        TokenStream::from(expanded)
+        });
+        expanded
     } else {
-        let expanded = quote::quote! {
+        let expanded: TokenStream = TokenStream::from(quote::quote! {
             compile_error!("this function does not accept any parameters")
-        };
-        TokenStream::from(expanded)
+        });
+        expanded
     }
 }
