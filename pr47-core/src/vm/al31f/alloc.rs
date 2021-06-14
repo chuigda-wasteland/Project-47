9aa87e020c61bf145bd83d6ec3403f1e6971f87c
icey <icey@icey.tech>
2021-06-14 20:27:28 +0800

    [ vm::al31f::alloc ] This `Alloc` trait should be fine

--- pr47-core/src/vm/al31f/alloc.rs
+++ pr47-core/src/vm/al31f/alloc.rs
@@ -1,8 +1,36 @@
+use std::collections::HashSet;
+
+use crate::data::Value;
+use crate::data::wrapper::DynBase;
 use crate::vm::al31f::stack::Stack;
+use crate::util::mem::FatPointer;
 
 pub trait Alloc {
-    fn add_stack(&mut self, stack: *const Stack);
-    fn collect(&mut self);
-
-    // TODO what API would we like for allocating?
+    unsafe fn add_stack(&mut self, stack: *const Stack<'_>);
+    unsafe fn remove_stack(&mut self, stack: *const Stack<'_>);
+    unsafe fn alloc(&mut self, data: *mut dyn DynBase) -> Value;
+    unsafe fn alloc_raw(&mut self, data: FatPointer) -> Value;
+    unsafe fn collect(&mut self);
 }
+
+pub struct DefaultAlloc {
+    stacks: HashSet<*const Stack<'static>>,
+    blks: HashSet<FatPointer>
+}
+
+impl DefaultAlloc {
+    pub fn new() -> Self {
+        Self {
+            stacks: HashSet::new(),
+            blks: HashSet::new()
+        }
+    }
+}
+
+// TODO
+// impl Drop for DefaultAlloc {
+//     ...
+// }
+// impl Alloc for DefaultAlloc {
+//     ...
+// }
