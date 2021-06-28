87aecc1ec3c9426bf11b159db4a7c51dd64c3bda
icey <icey@icey.tech>
2021-06-29 00:13:29 +0800

    [ vm::al31f ] merely modify `Alloc` trait

--- pr47-core/src/vm/al31f/alloc.rs
+++ pr47-core/src/vm/al31f/alloc.rs
@@ -4,14 +4,24 @@ use std::mem::transmute;
 use crate::vm::al31f::stack::Stack;
 use crate::util::mem::FatPointer;
 
+/// Abstract memory manager of `AL31F` engine
 pub trait Alloc {
+    /// Add one stack to `Alloc` management
     unsafe fn add_stack(&mut self, stack: *const Stack<'_>);
+    /// Remove one stack from `Alloc` management
     unsafe fn remove_stack(&mut self, stack: *const Stack<'_>);
+    /// Make the object denoted by `data` pointer managed
     unsafe fn add_managed(&mut self, data: FatPointer);
+    /// Mark the object denoted by `data` as useful when it gets added into some container. This
+    /// method is used by tri-color GC.
+    unsafe fn mark_object(&mut self, data: FatPointer);
+    /// Perform garbage collection
     unsafe fn collect(&mut self);
+    /// Allow or disallow garbage collection
     fn set_gc_allowed(&mut self, allowed: bool);
 }
 
+/// Default allocator for `AL31F`, with STW GC.
 pub struct DefaultAlloc {
     stacks: HashSet<*const Stack<'static>>,
     managed: HashSet<FatPointer>,
@@ -66,6 +76,10 @@ impl Alloc for DefaultAlloc {
         self.managed.insert(data);
     }
 
+    unsafe fn mark_object(&mut self, _data: FatPointer) {
+        // do nothing
+    }
+
     unsafe fn collect(&mut self) {
         let mut _to_collect: VecDeque<FatPointer> = VecDeque::new();
     }
@@ -74,4 +88,3 @@ impl Alloc for DefaultAlloc {
         self.gc_allowed = allowed;
     }
 }
-
--- pr47-core/src/vm/al31f/mod.rs
+++ pr47-core/src/vm/al31f/mod.rs
@@ -1,3 +1,12 @@
+use std::marker::PhantomData;
+
+use crate::vm::al31f::alloc::Alloc;
+
 pub mod alloc;
 pub mod stack;
 pub mod insc;
+
+pub struct AL31F<A: Alloc> {
+    // TODO don't know what kind of structure is really required
+    _phantom: PhantomData<A>
+}
