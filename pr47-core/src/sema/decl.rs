ed6f9d77182736c61835153734e4e3d00c6549ef
ICEY <icey@icey.tech>
2022-03-17 17:12:12 +0800

    [ sema ] Uh yes boy your ass is so tight

--- pr47-core/src/sema/decl.rs
+++ pr47-core/src/sema/decl.rs
@@ -3,17 +3,22 @@ use std::ptr::NonNull;
 use xjbutil::typed_arena::ArenaPtr;
 
 use crate::data::tyck::TyckInfo;
-use crate::syntax::decl::ConcreteDecl;
+use crate::syntax::decl::{ConcreteFuncDecl, ConcreteObjectDecl};
 
 pub struct ObjectDecl<'s> {
     pub name: &'s str,
     pub is_const: bool,
     pub ty: NonNull<TyckInfo>,
 
-    pub concrete: &'s ConcreteDecl<'s>
+    pub concrete: &'s ConcreteObjectDecl<'s>
 }
 
 pub struct FuncDecl<'s> {
     pub name: &'s str,
-    pub param_decls: Vec<ArenaPtr<ObjectDecl<'s>>>
+    pub param_decls: Vec<ArenaPtr<ObjectDecl<'s>>>,
+    pub ret_types: Vec<NonNull<TyckInfo>>,
+    pub exception_spec: Vec<NonNull<TyckInfo>>,
+    pub func_body: (), // TODO
+
+    pub concrete: &'s ConcreteFuncDecl<'s>
 }
